8ae67a1a2d5bb7fa37279e46934cd325c96c7865
max6555 <khanhtoan.le.6555@gmail.com>
2023-03-03 17:31:40 +0700

    complete crud functions - develop ui, ux task

--- PersonalInformationAPI/Controllers/PersonController.cs
+++ PersonalInformationAPI/Controllers/PersonController.cs
@@ -4,6 +4,9 @@ using PersonalInformationAPI.Model;
 using System.Collections.Generic;
 using System.Data;
 using System.Reflection.Metadata;
+using Newtonsoft.Json;
+using System;
+
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -13,7 +16,6 @@ namespace PersonalInformationAPI.Controllers
     [ApiController]
     public class PersonController : ControllerBase
     {
-
 
         // GET: api/<PersonController>
         [HttpGet]
@@ -45,24 +47,72 @@ namespace PersonalInformationAPI.Controllers
 
         // GET api/<PersonController>/5
         [HttpGet("{id}")]
-        public string Get(int id)
+        public Person Get(int id)
         {
-            return "value";
+            Person newPerson = null;
+            string connectionString = "Server=localhost;Port=3306;Database=personalinformation;Uid=root;Pwd=20150601;";
+            using (MySqlConnection conn = new MySqlConnection(connectionString))
+            {
+                conn.Open();
+                MySqlCommand cmd = new MySqlCommand($"select * from Person where personID = '{id}'", conn);
+                using (var reader = cmd.ExecuteReader(CommandBehavior.SingleRow))
+                {
+                    if (reader.Read()) {
+                        newPerson = new Person(
+                            (int)reader["PersonID"]
+                            , reader["firstName"].ToString()
+                            , reader["lastName"].ToString()
+                            , reader["address"].ToString()
+                            , reader["gender"].ToString()
+                            );
+                    }
+                }
+            }
+            return newPerson;
         }
 
         // POST api/<PersonController>
         [HttpPost]
-        public void Post([FromBody] Person person)
+        public string Post([FromBody] Person person)
         {
-            Random rnd = new Random();
-            person.personID = rnd.Next();
-            Console.WriteLine(person);
+            //Random rnd = new Random();
+            //person.personID = rnd.Next(0,100);
+            string connectionString = "Server=localhost;Port=3306;Database=personalinformation;Uid=root;Pwd=20150601;";
+            using (MySqlConnection conn = new MySqlConnection(connectionString))
+            {
+                conn.Open();
+                MySqlCommand cmd = new MySqlCommand($"insert into person values('{person.personID}','{person.firstName}','{person.lastName}','{person.address}','{person.gender}')", conn);
+                int changes = cmd.ExecuteNonQuery();
+                if (changes > 0)
+                {
+                    return "insert complete";
+                }
+                else
+                {
+                    return "there's something wrong";
+                }
+            }
         }
 
         // PUT api/<PersonController>/5
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
+        public string Put(int id, [FromBody] Person modifiedPerson)
         {
+            string connectionString = "Server=localhost;Port=3306;Database=personalinformation;Uid=root;Pwd=20150601;";
+            using (MySqlConnection conn = new MySqlConnection(connectionString))
+            {
+                conn.Open();
+                MySqlCommand cmd = new MySqlCommand($"update person set firstName = '{modifiedPerson.firstName}',lastName = '{modifiedPerson.lastName}',address='{modifiedPerson.address}',gender = '{modifiedPerson.gender}' where  personID = '{id}'", conn);
+                int changes = cmd.ExecuteNonQuery();
+                if (changes > 0)
+                {
+                    return "update complete";
+                }
+                else
+                {
+                    return "there's something wrong";
+                }
+            }
         }
 
         // DELETE api/<PersonController>/5
---
+++ PersonalInformationApp/CustomAuthenticationStateProvider.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Components.Authorization;
+
+namespace PersonalInformationApp
+{
+    public class CustomAuthenticationStateProvider : AuthenticationStateProvider
+    {
+        public override Task<AuthenticationState> GetAuthenticationStateAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
