702472e7c36e88bacaa60693819968b63c13ae0e
max6555 <khanhtoan.le.6555@gmail.com>
2023-03-08 17:32:51 +0700

    dummy message

--- PersonalInformationApp/Person.cs
+++ PersonalInformationApp/Person.cs
@@ -2,22 +2,11 @@
 {
     public class Person
     {
-        public int personID { get; set; }
-        public string firstName {get; set; }
-        public string lastName { get; set; }   
-        public string address { get; set; } 
-        public string gender { get; set; }
-        public Person()
-        {
+        public string Salutation { get; set; }
+        public string GivenName { get; set; }
+        public string FamilyName { get; set; }
 
-        }
-        public Person(int PersonID, string firstName, string lastName,string address, string gender)
-        {
-            this.personID = PersonID;
-            this.firstName = firstName;
-            this.lastName = lastName;
-            this.address = address;
-            this.gender = gender;
-        }
+
     }
+
 }
--- PersonalInformationApp/wwwroot/main.js
+++ PersonalInformationApp/wwwroot/main.js
@@ -1,14 +1,18 @@
 function Greeting() {
     console.log("the greeting from js");
 }
-window.downloadFileFromStream = async (fileName, contentStreamReference) => {
-    const arrayBuffer = await contentStreamReference.arrayBuffer();
-    const blob = new Blob([arrayBuffer]);
-    const url = URL.createObjectURL(blob);
-    const anchorElement = document.createElement('a');
-    anchorElement.href = url;
-    anchorElement.download = fileName ?? '';
-    anchorElement.click();
-    anchorElement.remove();
-    URL.revokeObjectURL(url);
+
+var MyElementReference = MyElementReference || {}
+MyElementReference.setFocus = function (element) {
+    console.log("call js function")
+    element.focus();
+}
+function AutoFocusFunction(element) {
+    console.log("Call js function");
+    element.focus();
+}
+function TheCallFromDotNet(dotnetObject) {
+    let greetingFromJS = "u did me wrong";
+    console.log("JS")
+    dotnetObject.invokeMethodAsync("ConsoleApp", greetingFromJS);
 }
